6a40eb2b9a50ceffa60e524e222333233c0543cd
Luiz Gustavo <luizg0509@gmail.com>
2024-05-09 17:15:07 -0300

    Falta só comentários

--- Q4/index.c
+++ Q4/index.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
-#define TNUM 9
 
 pthread_mutex_t mutex_cond = PTHREAD_MUTEX_INITIALIZER;
 int condicao = 1; // Valido é 1 e invalido é 0
@@ -13,22 +12,14 @@ struct Cords{
 
 void *verificar_grade(void *dados) {
         struct Cords crd = *((struct Cords*) dados);
-        // printf("Verificando %d-%d\n", crd.largura, crd.altura);
-        // verificar primeiro o 3x3 que ele iniciou
         int aux_3x3 = 0;
         for (int i = crd.largura; i < crd.largura + 3; i++) {
                 for (int j = crd.altura; j < crd.altura + 3; j++) {
-                        // fazer verificar a condiçao( se ja for invalido ele para)
-                        // printf("Grade_ver %d-%d\n", i,j);
                         if (!condicao) {
                                 pthread_exit(NULL);
-                                // return NULL;
                         }
                         int bit_3x3 = 1 << crd.sudoku[i][j];
-                        // printf("Verificando elemento %d[%d][%d]\n", crd.sudoku[i][j], i, j);
                         if(aux_3x3 & bit_3x3){
-                                // invalido
-                                // mudar variavel de condiçao, com mutex
                                 pthread_mutex_lock(&mutex_cond);
                                 condicao = 0;
                                 pthread_mutex_unlock(&mutex_cond);
@@ -41,10 +32,8 @@ void *verificar_grade(void *dados) {
                         for (int _j = 0; _j < 9; _j++) {
                                 if (!condicao) {
                                         pthread_exit(NULL);
-                                        // return NULL;
                                 }
                                 int bit_linha = 1 << crd.sudoku[i][_j];
-                                // printf("Linha ver %d-%d\n", i, _j);
                                 if(aux_linha & bit_linha){
                                         // invalido
                                         pthread_mutex_lock(&mutex_cond);
@@ -60,7 +49,6 @@ void *verificar_grade(void *dados) {
                         for (int _i = 0; _i < 9; _i++) {
                                 if (!condicao) {
                                         pthread_exit(NULL);
-                                        // return NULL;
                                 }
                                 int bit_coluna = 1 << crd.sudoku[_i][j];
                                 // printf("Coluna Ver %d-%d\n", _i, j);
@@ -77,7 +65,6 @@ void *verificar_grade(void *dados) {
                 }
 
         } 
-        printf("Terminando\n");
         pthread_exit(NULL);
 }
 
@@ -90,7 +77,7 @@ struct Cords Ccords(int** sudoku, int l, int a){
 };
 
 int main(){
-        pthread_t ids_threads[TNUM];
+        pthread_t ids_threads[3][3];
         // parte de ler a matriz pelo scanf  
         int** matrix = (int **)malloc(sizeof(int*) * 9);
         for (int i = 0; i < 9; i++) {
@@ -99,32 +86,21 @@ int main(){
                         scanf("%d ", &matrix[i][j]);
                 }
         }
-        struct Cords *cordenadas = malloc(sizeof(struct Cords) * 9);
-        // for (int i = 0; i < 3; i++) {
-        //         for (int j = 0; j < 3; j++) {
-        //                 cordenadas[i]  Ccords(matrix, 3 * i, 3 * j));
-        //                 // verificar_grade(Ccords(matrix, i * 3, j * 3));
-        //         }
-        // }
+        struct Cords **cordenadas = (struct Cords **)malloc(sizeof(struct Cords*) * 3);
 
-        cordenadas[0] = Ccords(matrix, 3 * 0, 3 * 0);
-        cordenadas[1] = Ccords(matrix, 3 * 1, 3 * 0);
-        cordenadas[2] = Ccords(matrix, 3 * 2, 3 * 0);
-        cordenadas[3] = Ccords(matrix, 3 * 0, 3 * 1);
-        cordenadas[4] = Ccords(matrix, 3 * 1, 3 * 1);
-        cordenadas[5] = Ccords(matrix, 3 * 2, 3 * 1);
-        cordenadas[6] = Ccords(matrix, 3 * 0, 3 * 2);
-        cordenadas[7] = Ccords(matrix, 3 * 1, 3 * 2);
-        cordenadas[8] = Ccords(matrix, 3 * 2, 3 * 2);
-        for (int i = 0; i < 9; i++) {
-             pthread_create(&ids_threads[i], NULL, verificar_grade, (void *)&cordenadas[i]);
+
+        for (int i = 0; i < 3; i++) {
+                cordenadas[i] = (struct Cords*) malloc(sizeof(struct Cords) * 3);
+                for (int j = 0; j < 3; j++) {
+                        cordenadas[i][j] = Ccords(matrix, 3 * i, 3 * j);
+                        pthread_create(&ids_threads[i][j], NULL, verificar_grade, (void *)&cordenadas[i][j]);
+                }
         }
-        for (int i = 0; i < TNUM; i++){
-                printf("Thread %d terminou\n", i);
-                pthread_join(ids_threads[i], NULL);
-}
-        // tava dando seg fault pois eu tava colocando o ponteiro direto no create da thread
-        // printf("Threads Terminaram\n");
+        for (int i = 0; i < 3; i++){
+                for (int j = 0; j < 3; j++) {
+                        pthread_join(ids_threads[i][j], NULL);
+                }
+        }
         if(condicao == 1){
                 printf("Sudoku Válido\n");
         } else {
@@ -135,6 +111,10 @@ int main(){
                 free(matrix[i]);
         }
         free(matrix);
+        for (int i = 0; i < 3; i++) {
+                free(cordenadas[i]);
+        }
+        free(cordenadas);
 
         return 0;
 }
