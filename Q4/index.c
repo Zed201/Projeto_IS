e589933f9884d19aa12656d469ec532e4f58b8b3
Luiz Gustavo <luizg0509@gmail.com>
2024-05-09 16:51:00 -0300

    Quase

--- Q4/index.c
+++ Q4/index.c
@@ -13,18 +13,25 @@ struct Cords{
 
 void *verificar_grade(void *dados) {
         struct Cords crd = *((struct Cords*) dados);
+        // printf("Verificando %d-%d\n", crd.largura, crd.altura);
         // verificar primeiro o 3x3 que ele iniciou
         int aux_3x3 = 0;
         for (int i = crd.largura; i < crd.largura + 3; i++) {
                 for (int j = crd.altura; j < crd.altura + 3; j++) {
                         // fazer verificar a condiçao( se ja for invalido ele para)
-                        if (condicao != 1) {
+                        // printf("Grade_ver %d-%d\n", i,j);
+                        if (!condicao) {
                                 pthread_exit(NULL);
+                                // return NULL;
                         }
                         int bit_3x3 = 1 << crd.sudoku[i][j];
+                        // printf("Verificando elemento %d[%d][%d]\n", crd.sudoku[i][j], i, j);
                         if(aux_3x3 & bit_3x3){
                                 // invalido
                                 // mudar variavel de condiçao, com mutex
+                                pthread_mutex_lock(&mutex_cond);
+                                condicao = 0;
+                                pthread_mutex_unlock(&mutex_cond);
 
                                 pthread_exit(NULL);
                         }
@@ -32,38 +39,53 @@ void *verificar_grade(void *dados) {
                         // verifica a linha do elemento
                         int aux_linha = 0;
                         for (int _j = 0; _j < 9; _j++) {
-                                if (condicao != 1) {
+                                if (!condicao) {
                                         pthread_exit(NULL);
+                                        // return NULL;
                                 }
                                 int bit_linha = 1 << crd.sudoku[i][_j];
+                                // printf("Linha ver %d-%d\n", i, _j);
                                 if(aux_linha & bit_linha){
                                         // invalido
+                                        pthread_mutex_lock(&mutex_cond);
+                                        condicao = 0;
+                                        pthread_mutex_unlock(&mutex_cond);
+
+                                        pthread_exit(NULL);
                                 }
                                 aux_linha |= bit_linha;
                         }
                         // verificar a coluna
                         int aux_coluna = 0;
                         for (int _i = 0; _i < 9; _i++) {
-                                if (condicao != 1) {
+                                if (!condicao) {
                                         pthread_exit(NULL);
+                                        // return NULL;
                                 }
                                 int bit_coluna = 1 << crd.sudoku[_i][j];
+                                // printf("Coluna Ver %d-%d\n", _i, j);
                                 if(aux_coluna & bit_coluna){
                                         // invalido
+                                        pthread_mutex_lock(&mutex_cond);
+                                        condicao = 0;
+                                        pthread_mutex_unlock(&mutex_cond);
+
+                                        pthread_exit(NULL);
                                 }
                                 aux_coluna |= bit_coluna;
                         }
                 }
 
         } 
+        printf("Terminando\n");
         pthread_exit(NULL);
 }
 
-struct Cords* Ccords(int** sudoku, int l, int a){
-        struct Cords* ptr = malloc(sizeof(struct Cords));
-        ptr->largura = l;
-        ptr->altura = a;
-        ptr->sudoku = sudoku;
+struct Cords Ccords(int** sudoku, int l, int a){
+        struct Cords ptr;
+        ptr.largura = l;
+        ptr.altura = a;
+        ptr.sudoku = sudoku;
         return ptr;
 };
 
@@ -77,21 +99,38 @@ int main(){
                         scanf("%d ", &matrix[i][j]);
                 }
         }
-
-        // fazer leitura dos arquivos(no caso pegar como scanf passando por > os dados)
-        // for (int i = 0; i < TNUM; i++) {
-        //         pthread_create(&ids_threads[i], NULL, /*funcao*/, /*Posicao de começar, fazer for duplo*/)
-        // }
-        // for (int i = 0; i < TNUM; i++){
-        //         pthread_join(ids_threads[i], NULL)
-        // }
-        // if(condição == 1){
-        //         printf("Sudoku Válido\n");
-        // } else {
-        //         printf("Sudoku Inválido\n");
+        struct Cords *cordenadas = malloc(sizeof(struct Cords) * 9);
+        // for (int i = 0; i < 3; i++) {
+        //         for (int j = 0; j < 3; j++) {
+        //                 cordenadas[i]  Ccords(matrix, 3 * i, 3 * j));
+        //                 // verificar_grade(Ccords(matrix, i * 3, j * 3));
+        //         }
         // }
 
-        verificar_grade(Ccords(matrix, 1, 2));
+        cordenadas[0] = Ccords(matrix, 3 * 0, 3 * 0);
+        cordenadas[1] = Ccords(matrix, 3 * 1, 3 * 0);
+        cordenadas[2] = Ccords(matrix, 3 * 2, 3 * 0);
+        cordenadas[3] = Ccords(matrix, 3 * 0, 3 * 1);
+        cordenadas[4] = Ccords(matrix, 3 * 1, 3 * 1);
+        cordenadas[5] = Ccords(matrix, 3 * 2, 3 * 1);
+        cordenadas[6] = Ccords(matrix, 3 * 0, 3 * 2);
+        cordenadas[7] = Ccords(matrix, 3 * 1, 3 * 2);
+        cordenadas[8] = Ccords(matrix, 3 * 2, 3 * 2);
+        for (int i = 0; i < 9; i++) {
+             pthread_create(&ids_threads[i], NULL, verificar_grade, (void *)&cordenadas[i]);
+        }
+        for (int i = 0; i < TNUM; i++){
+                printf("Thread %d terminou\n", i);
+                pthread_join(ids_threads[i], NULL);
+}
+        // tava dando seg fault pois eu tava colocando o ponteiro direto no create da thread
+        // printf("Threads Terminaram\n");
+        if(condicao == 1){
+                printf("Sudoku Válido\n");
+        } else {
+                printf("Sudoku Inválido\n");
+        }
+
         for (int i = 0; i < 9; i++) {
                 free(matrix[i]);
         }
