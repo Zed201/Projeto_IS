1fca58b4a7603bc16d67625b523f57f1e3196e52
Luiz Gustavo <luizg0509@gmail.com>
2024-05-24 18:47:54 -0300

    Funcionando para multiplos processos mas apenas algumas vezes

--- Q6/index.c
+++ Q6/index.c
@@ -6,7 +6,7 @@
 // a principio 1 para testar
 // TODO: Mudar o codigo para ele conseguir executar mais de uma
 #define time_testS 2
-#define N 1
+#define N 2 // se o N nao for compativel com a quantidade de processos, ele trava pois a propria fila trava
 #define quatum_ms 1 // quantum fixo, em testes ele difrete de um tempo bom para cada pc
 // com numeros pequenos de processamento ele simplesmente executa e para
 #define processamento 200
@@ -14,58 +14,69 @@ int flag_quantum = 0; // se 0 ele executa com o mesmo quantum de todos se 1 ele 
 fila* lista_pronto;
 
 void *escalonador_func(void* args){
-        pro* temp_pro;
-        int q = quatum_ms;
+        pro** temp_pro = (pro**) malloc(sizeof(pro*) * N);
+        int q = quatum_ms, t = time_testS;
         // ver alguma forma de para quando nao tiver mais processos na fila
         while(1){
-                temp_pro = pop(lista_pronto); // se a lista
+                for (int i = 0; i < N; i++) {        
+                        temp_pro[i] = pop(lista_pronto); // se a lista
+                        if(flag_quantum){
+                                // executar com quantum individual
+                                printf("Executando processo %s por %d ms\n", temp_pro[i]->Nome_Processo, temp_pro[i]->quantum);
+                        } else {
+                                // mesmo quantum para todos
+                                printf("Executando processo %s por %d ms\n", temp_pro[i]->Nome_Processo, q);
+                        }
 
-                if(flag_quantum){
-                        // executar com quantum individual
-                        printf("Executando processo %s por %d ms\n", temp_pro->Nome_Processo, temp_pro->quantum);
-                } else {
-                        // mesmo quantum para todos
-                        printf("Executando processo %s por %d ms\n", temp_pro->Nome_Processo, q);
                 }
-                printf("Espera 2s\n"); // novamente para nao ficar muito bagunçado o print
-                sleep(time_testS);
-                pthread_mutex_lock(&temp_pro->m2);
-                temp_pro->flag_exe = 0;
-                pthread_mutex_unlock(&temp_pro->m2);
 
-                pthread_cond_signal(&temp_pro->con);
+                printf("Espera %ds\n", t); // novamente para nao ficar muito bagunçado o print
+                sleep(t);
+                for(int i = 0; i < N; i++){
+                        pthread_mutex_lock(&temp_pro[i]->m2);
+                        temp_pro[i]->flag_exe = 0;
+                        pthread_mutex_unlock(&temp_pro[i]->m2);
+
+                        pthread_cond_signal(&temp_pro[i]->con);
+                }
 
                 if (flag_quantum) {
-                        usleep(1000 * temp_pro->quantum);
+                        // tirar o quantum variavel, pois com varios fica dificil sincronizar
+                        // usleep(1000 * temp_pro->quantum);
                 } else {
                         usleep(1000 * quatum_ms);
                 }
 
-                pthread_mutex_lock(&temp_pro->m2);
-                temp_pro->flag_exe = 1;
-                pthread_mutex_unlock(&temp_pro->m2);               
+                for(int i = 0; i < N; i++){
+                        pthread_mutex_lock(&temp_pro[i]->m2);
+                        temp_pro[i]->flag_exe = 1;
+                        pthread_mutex_unlock(&temp_pro[i]->m2);               
 
-                pthread_mutex_lock(&temp_pro->m1);
-                if(temp_pro->flag_end){
-                        printf("Processo %s finalizado\n", temp_pro->Nome_Processo);
-                        // liberar memoira
-                        pthread_mutex_destroy(&temp_pro->m1);
-                        pthread_mutex_destroy(&temp_pro->m2);
-                        pthread_cond_destroy(&temp_pro->con);
-                        free(temp_pro);
-                } else {
-                        push(lista_pronto, temp_pro);
-                        printf("Processo não terminou no tempo do quantum e voltara para o final da fila\nEsperando 2s\n");
-                        // espera para nao ficar muito bagunçado no print
-                        if(flag_quantum){
-                                // executar com quantum individual
-                                printf("Quantum aumentado\n");
-                                temp_pro->quantum *= 2;
-                        } 
+                        pthread_mutex_lock(&temp_pro[i]->m1);
+                        if(temp_pro[i]->flag_end){
+                                printf("Processo %s finalizado\n", temp_pro[i]->Nome_Processo);
+                                // liberar memoira
+                                pthread_mutex_destroy(&temp_pro[i]->m1);
+                                pthread_mutex_destroy(&temp_pro[i]->m2);
+                                pthread_cond_destroy(&temp_pro[i]->con);
+                                free(temp_pro[i]);
+                        } else {
+                                push(lista_pronto, temp_pro[i]);
+                                // temp_pro[i] = NULL;
+                                printf("Processo não terminou no tempo do quantum e voltara para o final da fila\nEsperando 2s\n");
+                                // espera para nao ficar muito bagunçado no print
+                                // tirar dps
+                                // if(flag_quantum){
+                                //         // executar com quantum individual
+                                //         printf("Quantum aumentado\n");
+                                //         temp_pro->quantum *= 2;
+                                // } 
 
-                        //sleep(time_testS);
+                                //sleep(time_testS);
+                        }
+
+                        pthread_mutex_unlock(&temp_pro[i]->m1);               
                 }
-                pthread_mutex_unlock(&temp_pro->m1);               
         }
         pthread_exit(NULL);
 }
@@ -98,13 +109,14 @@ void *generic_func(void *args){
 
 }
 
+#define pT 4
 int main(){
         initQueue(&lista_pronto);
         // testes-----------------------
-        pthread_t t[3];
-        char* nomes[3] = {"T1", "T2", "T3"};
+        pthread_t t[pT];
+        char* nomes[pT] = {"T1", "T2", "T3", "T4"};
         pro* temp;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < pT; i++) {
                 temp = process_create(nomes[i], processamento);
                 pthread_create(&temp->id, NULL, generic_func, temp);
                 push(lista_pronto, temp);
