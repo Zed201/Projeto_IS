aea9a91ac2544c61d2763c7aba95fde5391dc806
Luiz Gustavo <luizg0509@gmail.com>
2024-05-26 21:53:19 -0300

    Q6 resolvido

--- Q6/index.c
+++ Q6/index.c
@@ -3,11 +3,10 @@
 #include <unistd.h>
 #include <stdio.h>
 
-#define time_testS 2
-#define N 2 // se o N nao for compativel com a quantidade de processos, ele trava pois a propria fila trava
+#define time_testS 1 // tempo de espera para nao ficar bagunçado no print 
+#define N 3 // se o N nao for compativel com a quantidade de processos, ele trava pois a propria fila trava
 #define quatum_ms 1 // quantum fixo, em testes ele difrete de um tempo bom para cada pc
 // com numeros pequenos de processamento ele simplesmente executa e para
-int flag_quantum = 0; // se 0 ele executa com o mesmo quantum de todos se 1 ele vai aumentando o quantum por processo
 fila* lista_pronto;
 
 void *escalonador_func(void* args){
@@ -17,25 +16,25 @@ void *escalonador_func(void* args){
         while(1){
                 if (lista_pronto->lenght == 0) {
                         printf("Fila Vazio\n");
+                        pthread_mutex_lock(&lista_pronto->mutex);
                         pthread_cond_wait(&lista_pronto->cond, &lista_pronto->mutex); // dorme se a queue estiver vazia
+                        pthread_mutex_unlock(&lista_pronto->mutex);
                 }
+
                 for (int i = 0; i < N; i++) {        
                         // fazer alguma verificação de quantidade de != NULL
+
                         temp_pro[i] = pop(lista_pronto); // se a lista
-                        if(temp_pro[i] != NULL){
-                                if(flag_quantum){
-                                        // executar com quantum individual
-                                        printf("Executando processo %s por %d ms\n", temp_pro[i]->name, temp_pro[i]->quantum);
-                                } 
-                                else {
-                                        // mesmo quantum para todos
-                                        printf("Executando processo %s por %d ms\n", temp_pro[i]->name, q);
-                                }
+                        if(temp_pro[i] != NULL){ 
+                                // mesmo quantum para todos
+                                printf("Executando processo %s por %d ms\n", temp_pro[i]->name, q);
+
                         }
                 }
 
-                printf("Espera %ds\n", t); // novamente para nao ficar muito bagunçado o print
-                sleep(t);
+                // printf("Espera %ds\n", t); // novamente para nao ficar muito bagunçado o print
+                // sleep(t);
+                printf("------------------------------------\n");
                 for(int i = 0; i < N; i++){
                         if(temp_pro[i] != NULL){
                                 pthread_mutex_lock(&temp_pro[i]->m_exec);
@@ -46,15 +45,12 @@ void *escalonador_func(void* args){
                         }
                 }
 
-                if (flag_quantum) {
-                        // tirar o quantum variavel, pois com varios fica dificil sincronizar
-                        // usleep(1000 * temp_pro->quantum);
-                } else {
-                        usleep(1000 * quatum_ms);
-                }
+                usleep(1000 * quatum_ms);
 
                 for(int i = 0; i < N; i++){
                         if(temp_pro[i] != NULL){
+
+                printf("Iteração escalonador\n");
                                 pthread_mutex_lock(&temp_pro[i]->m_exec);
                                 temp_pro[i]->flag_exec = 1;
                                 pthread_mutex_unlock(&temp_pro[i]->m_exec);               
@@ -69,18 +65,8 @@ void *escalonador_func(void* args){
                                         free(temp_pro[i]);
                                 } 
                                 else {
-                                        printf("Processo %s não terminou no tempo do quantum e voltara para o final da fila\nEsperando 2s\n", temp_pro[i]->name);
+                                        printf("Processo %s não terminou no tempo do quantum e voltara para o final da fila\n", temp_pro[i]->name);
                                         push(lista_pronto, temp_pro[i]);
-                                        // temp_pro[i] = NULL;
-                                        // espera para nao ficar muito bagunçado no print
-                                        // tirar dps
-                                        // if(flag_quantum){
-                                        //         // executar com quantum individual
-                                        //         printf("Quantum aumentado\n");
-                                        //         temp_pro->quantum *= 2;
-                                        // } 
-
-                                        //sleep(time_testS);
                                 }
 
                                 pthread_mutex_unlock(&temp_pro[i]->m_end);               
@@ -98,19 +84,25 @@ void *generic_func(void *args){
         // a propria thread tem acesso à sua estrutura de processo
         process* p_data = (process* ) args;
         printf("Processo de nome %s iniciado\n", p_data->name);
+        pthread_mutex_lock(&p_data->m_end);
         pthread_cond_wait(&p_data->con, &p_data->m_end);
+        pthread_mutex_unlock(&p_data->m_end);
         for (int i = 0; i < p_data->exec_qtd; i++) {
 
+                // pthread_mutex_lock(&p_data->m_end);
                 if(p_data->flag_exec){
-                        pthread_cond_wait(&p_data->con, &p_data->m_end);
-                        pthread_mutex_unlock(&p_data->m_end);
+                        // trocar para exec os mutex
+                        // pthread_cond_wait(&p_data->con, &p_data->m_end);
+                        pthread_mutex_lock(&p_data->m_exec);
+                        pthread_cond_wait(&p_data->con, &p_data->m_exec);
+                        pthread_mutex_unlock(&p_data->m_exec);
                 }
+                // pthread_mutex_unlock(&p_data->m_end);
 
                 printf("%s-%d\n", p_data->name, i);
                 fflush(stdout);
         }
-
-        pthread_mutex_unlock(&p_data->m_end); // alguns casos ele sai do for, se o p_data->exec_qtd for pequeno, com esse mutex locked e trava tudo
+        // pthread_mutex_unlock(&p_data->m_end); // alguns casos ele sai do for, se o p_data->exec_qtd for pequeno, com esse mutex locked e trava tudo
         pthread_mutex_lock(&p_data->m_end);
         p_data->flag_end = 1;
         pthread_mutex_unlock(&p_data->m_end);
@@ -124,7 +116,7 @@ int main(){
         // testes-----------------------
         pthread_t t[pT];
         char* nomes[pT] = {"T1", "T2", "T3", "T4"};
-        int pros[pT] = {300, 600, 700, 200};
+        int pros[pT] = {800, 900, 200, 700};
         process* temp;
         for (int i = 0; i < pT; i++) {
                 temp = process_create(nomes[i], pros[i]);
--- Q6/queue_safe.h
+++ Q6/queue_safe.h
@@ -12,124 +12,125 @@ typedef struct process process;
 // tentar ver de colocar status
 
 struct process {
-    char *name;
-    pthread_cond_t con;
-    pthread_mutex_t m_exec, m_end;
-    pthread_t id;
-    int flag_exec, flag_end;
-    int exec_qtd, quantum;
+        char *name;
+        pthread_cond_t con;
+        pthread_mutex_t m_exec, m_end;
+        pthread_t id;
+        int flag_exec, flag_end;
+        int exec_qtd, quantum;
 
-    // colocar o pthread_t aqui tbm
+        // colocar o pthread_t aqui tbm
 };
 
 // TODO: Testar para ver se essa funcao funciona
 process* process_create(char *nome, int qtd) {
-    struct process *ptr = (struct process*) malloc(sizeof(struct process));
-    ptr->name = nome;
-    ptr->flag_end = 0;
-    // ver onde coloca o destroy, talvez na hora da struct tiver finalizado a função
-    // dar alguma quantidade aleatoria de exec, tem que ser bastante alto
-    ptr->exec_qtd = qtd; // tamanho do loop fixo para todos basicamente
-    ptr->quantum = 10; // inicia com o padroa de 10 e vai aumentando se ele nao terminar
+        struct process *ptr = (struct process*) malloc(sizeof(struct process));
+        ptr->name = nome;
+        ptr->flag_end = 0;
+        // ver onde coloca o destroy, talvez na hora da struct tiver finalizado a função
+        // dar alguma quantidade aleatoria de exec, tem que ser bastante alto
+        ptr->exec_qtd = qtd; // tamanho do loop fixo para todos basicamente
+        ptr->quantum = 10; // inicia com o padroa de 10 e vai aumentando se ele nao terminar
 
-    pthread_cond_init(&ptr->con, NULL);
-    pthread_mutex_init(&ptr->m_exec, NULL);
-    pthread_mutex_init(&ptr->m_end, NULL);
-    
-    return ptr;
+        pthread_cond_init(&ptr->con, NULL);
+        pthread_mutex_init(&ptr->m_exec, NULL);
+        pthread_mutex_init(&ptr->m_end, NULL);
+
+        return ptr;
 }
 
 struct node
 {
-    process *data; // operacao guardada no nó
-    node *next;   // ponteiro para a proxima operacao
+        process *data; // operacao guardada no nó
+        node *next;   // ponteiro para a proxima operacao
 };
 
 struct queue
 {
-    node *head;            // operacao mais antiga
-    node *tail;            // operacao mais recente
-    int lenght;            // quantidade de operacoes na fila
-    pthread_mutex_t mutex; // mutex para impedir condicao de corrida
-    pthread_cond_t cond;   // condicao para acordar quem estiver esperando a fila possuir requisicoes (banco)
+        node *head;            // operacao mais antiga
+        node *tail;            // operacao mais recente
+        int lenght;            // quantidade de operacoes na fila
+        pthread_mutex_t mutex; // mutex para impedir condicao de corrida
+        pthread_cond_t cond;   // condicao para acordar quem estiver esperando a fila possuir requisicoes (banco)
 };
 
 // inicializador da fila
 void initQueue(fila **q)
 {
-    fila *new_queue = (fila *)malloc(sizeof(fila));
-    new_queue->head = NULL;
-    new_queue->tail = NULL;
-    new_queue->lenght = 0;
+        fila *new_queue = (fila *)malloc(sizeof(fila));
+        new_queue->head = NULL;
+        new_queue->tail = NULL;
+        new_queue->lenght = 0;
 
-    pthread_mutex_init(&new_queue->mutex, NULL); // inicia os mutex
-    pthread_cond_init(&new_queue->cond, NULL);   // inicia as condicoes
-    *q = new_queue;
+        pthread_mutex_init(&new_queue->mutex, NULL); // inicia os mutex
+        pthread_cond_init(&new_queue->cond, NULL);   // inicia as condicoes
+        *q = new_queue;
 }
 
 // operacao atomica para adicionar uma nova operacao na fila
 void push(fila *q, process *dado)
 {
-    // criacao da requisicao a ser adicionada na queue
-    node *new_node;
-    new_node = (node *)malloc(sizeof(node));
-    new_node->next = NULL;
-    new_node->data = dado;
+        // criacao da requisicao a ser adicionada na queue
+        node *new_node;
+        new_node = (node *)malloc(sizeof(node));
+        new_node->next = NULL;
+        new_node->data = dado;
 
-    // new_node->data = (process *)malloc(sizeof(process));
-    
-    // strcpy(new_node->data->name, name);
-    // new_node->proc->status = ready;
-    // pthread_cond_init(&new_node->proc->cond, NULL);
+        // new_node->data = (process *)malloc(sizeof(process));
 
-    pthread_mutex_lock(&(q->mutex)); // lock necessario a partir do momento que se deseja alterar a queue q
-    if (q->tail != NULL)
-    {
-        q->tail->next = new_node;
-    }
-    q->tail = new_node;
+        // strcpy(new_node->data->name, name);
+        // new_node->proc->status = ready;
+        // pthread_cond_init(&new_node->proc->cond, NULL);
 
-    if (q->head == NULL)
-    {
-        q->head = new_node;
-    }
+        pthread_mutex_lock(&(q->mutex)); // lock necessario a partir do momento que se deseja alterar a queue q
+        if (q->tail != NULL)
+        {
+                q->tail->next = new_node;
+        }
+        q->tail = new_node;
 
-    q->lenght++;
-    // talvez trocar para brodcast
-    pthread_cond_signal(&q->cond);     // acorda o quem estiver dormindo na condicao (geramente o banco)
-    pthread_mutex_unlock(&(q->mutex)); // libera o mutex
+        if (q->head == NULL)
+        {
+                q->head = new_node;
+        }
+
+        q->lenght++;
+        // talvez trocar para brodcast
+        pthread_cond_signal(&q->cond);     // acorda o quem estiver dormindo na condicao (geramente o banco)
+        pthread_mutex_unlock(&(q->mutex)); // libera o mutex
 }
 
 // operacao atomica para receber uma operacao da fila
 process *pop(fila *q)
 {
-    process *value; // operacao que ira ser obtida da queue
+        process *value; // operacao que ira ser obtida da queue
 
-    pthread_mutex_lock(&(q->mutex)); // trava o mutex da queue se estiver livre
-    while (q->lenght == 0)
-    {
-        // printf("Fila vazia esperando processos\n");
-        // pthread_cond_wait(&q->cond, &q->mutex); // dorme se a queue estiver vazia
-        return NULL;
-    }
+        pthread_mutex_lock(&(q->mutex)); // trava o mutex da queue se estiver livre
+        while (q->lenght == 0)
+        {
+                // printf("Fila vazia esperando processos\n");
+                // pthread_cond_wait(&q->cond, &q->mutex); // dorme se a queue estiver vazia
+                pthread_mutex_unlock(&(q->mutex));
+                return NULL;
+        }
 
-    // obtem o primeiro item da queue
-    value = q->head->data;
+        // obtem o primeiro item da queue
+        value = q->head->data;
 
-    node *tmp_node = q->head;
+        node *tmp_node = q->head;
 
-    if (q->head == q->tail)
-    {
-        q->tail = NULL;
-    }
+        if (q->head == q->tail)
+        {
+                q->tail = NULL;
+        }
 
-    q->lenght--;
-    q->head = q->head->next;
-    free(tmp_node);
+        q->lenght--;
+        q->head = q->head->next;
+        free(tmp_node);
 
-    pthread_mutex_unlock(&(q->mutex)); // libera o mutex da queue
+        pthread_mutex_unlock(&(q->mutex)); // libera o mutex da queue
 
-    return value;
+        return value;
 }
 
 #endif
